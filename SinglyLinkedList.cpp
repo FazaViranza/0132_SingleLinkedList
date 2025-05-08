194d837772274e0556c5fac7b49450ef75d3de38
FazaViranza <nofila.faza.ft24@mail.umy.ac.id>
2025-05-08 17:34:37 +0700

    testo

--- SinglyLinkedList.cpp
+++ SinglyLinkedList.cpp
@@ -107,3 +107,20 @@ public:
         }
     }
 };
+
+int main ()
+{
+    LinkedList mhs;
+    int nim;
+    char ch;
+    do
+    {
+        cout <<"Menu" << endl;
+        cout << "1. Menambah data kedalam list" << endl;
+        cout << "2. Menghapus data dari halaman list" <<endl;
+        cout << "3. Menampilkan data dalam list" << endl;
+        cout << "4. Mencari data dalam list" << endl;
+        cout << "5. Keluar " << endl;
+        cout << endl
+             << "Masukan pilihan (1-5): " << endl;
+        cin >> ch;
